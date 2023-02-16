4f38f50283d8a208d1b582571c6686c2e203e9cd
ashwicket <124555370+ashwicket@users.noreply.github.com>
2023-02-16 08:50:29 +0530

    Wave Spawner added

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public TextMeshProUGUI waveCountdownText;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+
+    // Start is called before the first frame update
+    /* void Start()
+    {
+        
+    } */
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Floor(countdown + 1).ToString();
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveIndex++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
