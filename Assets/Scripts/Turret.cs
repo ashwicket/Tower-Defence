699b08399f646b113925af669add865752bc7215
ashwicket <124555370+ashwicket@users.noreply.github.com>
2023-02-16 18:10:43 +0530

    Created Turrets

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Turret : MonoBehaviour
+    {
+        public float range = 15f;
+        public Transform partToRotate;
+        public float turretSpeed = 10f;
+
+        private Transform target = null;
+        private string enemyTag = "Enemy";
+
+        // Use this for initialization
+        void Start()
+        {
+            InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        }
+
+        private void UpdateTarget()
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnemy = null;
+
+            foreach (GameObject enemy in enemies)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+                if (distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy = enemy;
+                }
+            }
+
+            if (nearestEnemy != null && shortestDistance <= range)
+            {
+                Debug.Log($"Target Found: {target}");
+                target = nearestEnemy.transform;
+            }
+            else
+            {
+                target = null;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (target == null)
+                return;
+
+            Vector3 dir = target.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turretSpeed).eulerAngles;
+            partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
+    }
+}
